3c5f2a17ec787ccb97aaefdd39fe3ee5ab5cf916
brian.dooley@feedhenry.com <brian.dooley@feedhenry.com>
2013-02-26 12:13:36 +0000

    update file /cloud/main.js

--- cloud/main.js
+++ cloud/main.js
@@ -6,3 +6,18 @@ exports.myFunction = function(params, callback) {
   callback(undefined, {result: num});
 };
 
+exports.greenFunction = function(params, callback) {
+  var num = params.num;
+  if(num > 10){
+    return callback({err: 'Num too large'}, {result: num});
+  }
+  callback(undefined, {result: num});
+};
+
+exports.redFunction = function(params, callback) {
+  var num = params.num;
+  if(num > 10){
+    return callback({err: 'Num too large'}, {result: num});
+  }
+  callback(undefined, {result: num});
+};
